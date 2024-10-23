455063cc15dd455e0aad84cd01dd84e80645a7e3
manjinder-mckc <manjinder_singh@mckinsey.com>
2024-10-23 10:36:39 -0400

    backend

--- abinbev_app/versions.tf
+++ abinbev_app/versions.tf
@@ -1,4 +1,12 @@
 terraform {
+
+  backend "azurerm" {
+    resource_group_name  = "dummy-rg"
+    storage_account_name = "dummysaab"
+    container_name       = "abappterraformstate"
+    key                  = "abinbev_app.tfstate"
+  }
+
   required_version = ">= 1.3, < 2"
 
   required_providers {
